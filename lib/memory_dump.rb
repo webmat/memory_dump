9d878bd182af3e0ca6e4bdc4f30f1b3bacdd7306
Mathieu Martin <webmat@gmail.com>
2008-10-02 18:28:15 -0400

    Initial commit. Can already dump all local, instance, global and class variables, as well as constants

---
+++ lib/memory_dump.rb
@@ -0,0 +1,27 @@
+require 'ruby-debug'
+require 'pp'
+module MemoryDump
+  VARIABLE_TYPES = {
+    :local_variables    => nil, 
+    :instance_variables => nil, 
+    :global_variables   => nil,
+    :class_variables    => 'self.class.class_variables',
+    :constants          => 'Module.constants'
+  }
+  VARIABLE_TYPES.each_pair{|k,v| VARIABLE_TYPES[k] = k.to_s unless v}
+  
+  NOISY = [:global_variables, :constants]
+  
+  def dump(b)
+    info = VARIABLE_TYPES.reject{|k,v| NOISY.include? k}.inject({}) do |memo, pair|
+      what, how = *pair
+      memo[what] = fetch how, b
+      memo
+    end 
+    pp info
+  end
+  
+  def fetch(what, b)
+    (eval what, b).inject({}) {|memo, var| memo[var] = eval var, b; memo}
+  end
+end
