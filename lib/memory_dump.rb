42d7cd6bf750a7bf3e60eddee2d1e4053f5899df
Mathieu Martin <webmat@gmail.com>
2008-10-02 21:17:10 -0400

    Bunch of clarifications, minor bug fixes

--- lib/memory_dump.rb
+++ lib/memory_dump.rb
@@ -3,41 +3,51 @@ require 'pp'
 
 module MemoryDump
   NESTED_INFO = {
-    :local_variables    => nil,
-    :instance_variables => nil,
-    :global_variables   => nil,
+    # what gets fetched => how it gets fetched
+    :local_variables    => 'local_variables',
+    :instance_variables => 'instance_variables',
+    :global_variables   => 'global_variables',
     :class_variables    => 'self.class.class_variables',
     :constants          => 'Module.constants'
   }
-  NESTED_INFO.each_pair{|k,v| NESTED_INFO[k] = k.to_s unless v}
   
   FLAT_INFO = {
-    :class              => 'self.class'
+    :class              => 'self.class.to_s'
   }
-  FLAT_INFO.each_pair{|k,v| NESTED_INFO[k] = k.to_s unless v}
   
   NOISY = [:global_variables, :constants]
   
-  # Just call 'dump'
-  def dump(b=binding)
-    debugger
-    info = NESTED_INFO.reject{|k,v| NOISY.include? k}.inject({}) { |memo, pair|
-      what, how = *pair
+  # You can call dump in one of two ways
+  #   dump binding
+  # or
+  #   dump {}
+  def dump(b=nil, &block)
+    case
+    when block_given?
+      b=block.binding
+    when b.is_a?(Binding)
+    else
+      raise ArgumentError, "Either pass 'binding' as the only argument to dump or pass an empty block"
+    end
+    
+    info_nested  = NESTED_INFO.reject{|k,v| NOISY.include? k}.inject({}) { |memo, pair|
+      what, how  = *pair
       memo[what] = fetch_nested how, b
       memo
-    }.merge(FLAT_INFO.inject({}) { |memo, pair|
-      what, how = *pair
+    }
+    info_flat    = FLAT_INFO.inject({}) { |memo, pair|
+      what, how  = *pair
       memo[what] = fetch_flat how, b
       memo
-    }) # <= Hey, like Javascript!
-    pp info
+    }
+    return info_nested.merge(info_flat)
   end
   
-  def fetch_flat(what, b)
-    eval what, b
+  def fetch_flat(how, b)
+    eval how, b
   end
   
-  def fetch_nested(what, b)
-    (eval what, b).inject({}) {|memo, var| memo[var] = eval var, b; memo}
+  def fetch_nested(how, b)
+    (eval how, b).inject({}) {|memo, var| memo[var] = eval var, b; memo}
   end
 end
