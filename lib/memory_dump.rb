b4ec37b22efc615662dd5c1602d933f9242261ba
Mathieu Martin <webmat@gmail.com>
2008-10-02 20:45:11 -0400

    Move comment to readme

--- lib/memory_dump.rb
+++ lib/memory_dump.rb
@@ -1,32 +1,43 @@
 require 'ruby-debug'
 require 'pp'
 
-# To know more about bindings, check out
-# http://onestepback.org/index.cgi/Tech/Ruby/RubyBindings.rdoc
-
 module MemoryDump
-  VARIABLE_TYPES = {
-    :local_variables    => nil, 
-    :instance_variables => nil, 
+  NESTED_INFO = {
+    :local_variables    => nil,
+    :instance_variables => nil,
     :global_variables   => nil,
     :class_variables    => 'self.class.class_variables',
     :constants          => 'Module.constants'
   }
-  VARIABLE_TYPES.each_pair{|k,v| VARIABLE_TYPES[k] = k.to_s unless v}
+  NESTED_INFO.each_pair{|k,v| NESTED_INFO[k] = k.to_s unless v}
+  
+  FLAT_INFO = {
+    :class              => 'self.class'
+  }
+  FLAT_INFO.each_pair{|k,v| NESTED_INFO[k] = k.to_s unless v}
   
   NOISY = [:global_variables, :constants]
   
   # Just call 'dump'
   def dump(b=binding)
-    info = VARIABLE_TYPES.reject{|k,v| NOISY.include? k}.inject({}) do |memo, pair|
+    debugger
+    info = NESTED_INFO.reject{|k,v| NOISY.include? k}.inject({}) { |memo, pair|
       what, how = *pair
-      memo[what] = fetch how, b
+      memo[what] = fetch_nested how, b
       memo
-    end 
+    }.merge(FLAT_INFO.inject({}) { |memo, pair|
+      what, how = *pair
+      memo[what] = fetch_flat how, b
+      memo
+    }) # <= Hey, like Javascript!
     pp info
   end
   
-  def fetch(what, b)
+  def fetch_flat(what, b)
+    eval what, b
+  end
+  
+  def fetch_nested(what, b)
     (eval what, b).inject({}) {|memo, var| memo[var] = eval var, b; memo}
   end
 end
